efc8960601d64864e793557eb10512d416ee1e63
Jordan Miller <Jordan.kay@gmail.com>
2023-12-31 12:31:56 -0700

    debug

--- satorilib/asynchronous/thread.py
+++ satorilib/asynchronous/thread.py
@@ -63,7 +63,6 @@ class AsyncThread():
     async def dailyWrapper(self, *args, func: callable, times: list[str], **kwargs):
         while True:
             now = dt.datetime.utcnow()
-            logging.debug('utcnow', now, color='magenta')
             nextRunTime = None
             for timeString in times:
                 runTime = (
@@ -73,9 +72,7 @@ class AsyncThread():
                     runTime += dt.timedelta(days=1)
                 if nextRunTime is None or runTime < nextRunTime:
                     nextRunTime = runTime
-            logging.debug('nextRunTime', nextRunTime, color='magenta')
             delay = (nextRunTime - now).total_seconds()
-            logging.debug('delay', delay, color='magenta')
             await asyncio.sleep(delay)
             try:
                 await self.asyncWrapper(*args, func=func, **kwargs)
