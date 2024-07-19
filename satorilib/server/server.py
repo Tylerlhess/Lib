78bfb6b1623ce99fa098db97f0bff1120b5071bd
Jordan Miller <Jordan.kay@gmail.com>
2024-07-18 22:47:35 -0600

    increase the speed of prediction

--- satorilib/server/server.py
+++ satorilib/server/server.py
@@ -385,7 +385,7 @@ class SatoriServerClient(object):
         isPrediction: bool = True,
     ) -> Union[bool, None]:
         ''' publish predictions '''
-        if isPrediction and self.topicTime.get(topic, 0) > time.time() - 60*60*6:
+        if isPrediction and self.topicTime.get(topic, 0) > time.time() - 60*60:
             return
         self.setTopicTime(topic)
         try:
